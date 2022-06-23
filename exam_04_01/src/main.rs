2d48a0f6cd3f33115d85e93594d06aad6b78f058
Rong Nguyen <khacrong@gmail.com>
2022-06-23 11:03:40 +0700

    20220623 commit course 04

--- exam_04_01/src/main.rs
+++ exam_04_01/src/main.rs
@@ -31,9 +31,9 @@
 // Yêu cầu: Sửa lỗi Lifetime 
 
 use std::fmt;
-struct StrDisplayable(Vec<String>);
+struct StrDisplayable<'a>(Vec<&'a str>);
 
-impl fmt::Display for StrDisplayable {
+impl<'a> fmt::Display for StrDisplayable<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for v in &self.0 {
             write!(f, "\n{}", v)?;
@@ -43,7 +43,7 @@ impl fmt::Display for StrDisplayable {
 }
 
 fn main() {
-    let vec: Vec<String> = vec!["a".to_string(),"bc".to_string(),"def".to_string()];
+    let vec: Vec<&str> = vec!["a","bc","def"];
     let vec_Foo = StrDisplayable(vec);
     println!("{}",vec_Foo);
 }
