a465e127e2f47a7c671ff6bc0761956a08176358
Rong Nguyen <khacrong@gmail.com>
2022-07-14 15:36:32 +0700

    20220714 commit course 01

---
+++ practice_week01/pallets/kitties/src/lib.rs
@@ -0,0 +1,185 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(test)]
+pub mod mock;
+
+#[cfg(test)]
+mod tests;
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{
+		pallet_prelude::*,
+		traits::{tokens::ExistenceRequirement, Currency, Randomness},
+	};
+	use frame_system::pallet_prelude::*;
+	use scale_info::TypeInfo;
+	use sp_io::hashing::blake2_128;
+	use sp_runtime::ArithmeticError;
+
+	#[cfg(feature = "std")]
+	use frame_support::serde::{Deserialize, Serialize};
+
+	type BalanceOf<T> =
+		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	#[scale_info(skip_type_params(T))]
+	pub struct Kitty<T: Config> {
+		pub dna: [u8; 16],
+		pub price: Option<BalanceOf<T>>,
+		pub gender: Gender,
+		pub owner: T::AccountId,
+	}
+
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+	pub enum Gender {
+		Male,
+		Female,
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type Currency: Currency<Self::AccountId>;
+		#[pallet::constant]
+		type MaxKittiesOwned: Get<u32>;
+		type KittyRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		TooManyOwned,
+		TransferToSelf,
+		DuplicateKitty,
+		NoKitty,
+		NotOwner,
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		Created { kitty: [u8; 16], owner: T::AccountId },
+		Transferred { from: T::AccountId, to: T::AccountId, kitty: [u8; 16] },
+	}
+
+	#[pallet::storage]
+	pub(super) type CountForKitties<T: Config> = StorageValue<_, u64, ValueQuery>;
+
+	#[pallet::storage]
+	pub(super) type Kitties<T: Config> = StorageMap<_, Twox64Concat, [u8; 16], Kitty<T>>;
+
+	#[pallet::storage]
+	pub(super) type KittiesOwned<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		T::AccountId,
+		BoundedVec<[u8; 16], T::MaxKittiesOwned>,
+		ValueQuery,
+	>;
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(0)]
+		pub fn create_kitty(origin: OriginFor<T>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let (kitty_gen_dna, gender) = Self::gen_dna();
+			Self::hp_mint(&sender, kitty_gen_dna, gender)?;
+			Ok(())
+		}		
+		
+		#[pallet::weight(0)]
+		pub fn transfer(
+			origin: OriginFor<T>,
+			to: T::AccountId,
+			kitty_id: [u8; 16],
+		) -> DispatchResult {
+			let from = ensure_signed(origin)?;
+			let kitty = Kitties::<T>::get(&kitty_id).ok_or(Error::<T>::NoKitty)?;
+			ensure!(kitty.owner == from, Error::<T>::NotOwner);
+			Self::hp_transfer(kitty_id, to)?;
+			Ok(())
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		pub fn gen_dna() -> ([u8; 16], Gender) {
+			let random = T::KittyRandomness::random(&b"dna"[..]).0;
+			let unique_payload = (
+				random,
+				frame_system::Pallet::<T>::extrinsic_index().unwrap_or_default(),
+				frame_system::Pallet::<T>::block_number(),
+			);
+
+			let encoded_payload = unique_payload.encode();
+			let hash = blake2_128(&encoded_payload);
+
+			if hash[0] % 2 == 0 {
+				(hash, Gender::Male)
+			} else {
+				(hash, Gender::Female)
+			}
+		}
+
+		pub fn hp_mint(
+			owner: &T::AccountId,
+			dna: [u8; 16],
+			gender: Gender,
+		) -> Result<[u8; 16], DispatchError> {
+			let kitty = Kitty::<T> { dna, price: None, gender, owner: owner.clone() };
+
+			ensure!(!Kitties::<T>::contains_key(&kitty.dna), Error::<T>::DuplicateKitty);
+
+			let count = CountForKitties::<T>::get();
+			let new_count = count.checked_add(1).ok_or(ArithmeticError::Overflow)?;
+
+			KittiesOwned::<T>::try_append(&owner, kitty.dna)
+				.map_err(|_| Error::<T>::TooManyOwned)?;
+
+			Kitties::<T>::insert(kitty.dna, kitty);
+			CountForKitties::<T>::put(new_count);
+
+			Self::deposit_event(Event::Created { kitty: dna, owner: owner.clone() });
+
+			Ok(dna)
+		}
+
+		pub fn hp_transfer(
+			kitty_id: [u8; 16],
+			to: T::AccountId,
+		) -> DispatchResult {
+			let mut kitty = Kitties::<T>::get(&kitty_id).ok_or(Error::<T>::NoKitty)?;
+			let from = kitty.owner;
+
+			ensure!(from != to, Error::<T>::TransferToSelf);
+			let mut from_owned = KittiesOwned::<T>::get(&from);
+
+			if let Some(ind) = from_owned.iter().position(|&id| id == kitty_id) {
+				from_owned.swap_remove(ind);
+			} else {
+				return Err(Error::<T>::NoKitty.into());
+			}
+
+			let mut to_owned = KittiesOwned::<T>::get(&to);
+			to_owned.try_push(kitty_id).map_err(|()| Error::<T>::TooManyOwned)?;
+
+			kitty.owner = to.clone();
+			kitty.price = None;
+
+			Kitties::<T>::insert(&kitty_id, kitty);
+			KittiesOwned::<T>::insert(&to, to_owned);
+			KittiesOwned::<T>::insert(&from, from_owned);
+
+			Self::deposit_event(Event::Transferred { from, to, kitty: kitty_id });
+
+			Ok(())
+		}
+	}
+}
